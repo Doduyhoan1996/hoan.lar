bb143685f02d525c3936e9fa6233d6896f6925cc
Admin <Admin>
2019-06-25 17:12:15 +0700

    baitap 22/06

--- authen/resources/views/admin/partials/sidebar.blade.php
+++ authen/resources/views/admin/partials/sidebar.blade.php
@@ -9,27 +9,99 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <h1><a class="navbar-brand" href="index.html"><span class="fa fa-area-chart"></span> Glance<span class="dashboard_text">Design dashboard</span></a></h1>
+                <h1><a class="navbar-brand" href="{{url('/')}}"><span class="fa fa-area-chart"></span> Glance<span class="dashboard_text">Design dashboard</span></a></h1>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="sidebar-menu">
                     <li class="header">MAIN NAVIGATION</li>
                     <li class="treeview">
-                        <a href="index.html">
+                        <a href="{{url('/admin')}}">
                             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                         </a>
                     </li>
                     <li class="treeview">
                         <a href="#">
                             <i class="fa fa-laptop"></i>
-                            <span>Components</span>
+                            <span>Shop</span>
                             <i class="fa fa-angle-left pull-right"></i>
                         </a>
                         <ul class="treeview-menu">
-                            <li><a href="grids.html"><i class="fa fa-angle-right"></i> Grids</a></li>
-                            <li><a href="media.html"><i class="fa fa-angle-right"></i> Media Css</a></li>
+                            <li><a href="{{url('/admin/shop/category')}}"><i class="fa fa-angle-right"></i> Danh mục</a></li>
+                            <li><a href="{{url('/admin/shop/product')}}"><i class="fa fa-angle-right"></i> Sản phẩm</a></li>
+                            <li><a href="{{url('/admin/shop/order')}}"><i class="fa fa-angle-right"></i> Đơn hàng</a></li>
+                            <li><a href="{{url('/admin/shop/review')}}"><i class="fa fa-angle-right"></i> Đánh giá</a></li>
+                            <li><a href="{{url('/admin/shop/customer')}}"><i class="fa fa-angle-right"></i> Khách hàng</a></li>
                         </ul>
                     </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-edit"></i>
+                            <span>Order</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{url('/admin/shop/product/order')}}"><i class="fa fa-angle-right"></i> Đặt sản phẩm</a></li>
+                        </ul>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-table"></i>
+                            <span>Content</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{url('/admin/content/category')}}"><i class="fa fa-angle-right"></i> Danh mục</a></li>
+                            <li><a href="{{url('/admin/content/post')}}"><i class="fa fa-angle-right"></i> Bài viết</a></li>
+                            <li><a href="{{url('/admin/content/page')}}"><i class="fa fa-angle-right"></i> Trang</a></li>
+                            <li><a href="{{url('/admin/content/tag')}}"><i class="fa fa-angle-right"></i> Tag</a></li>
+                        </ul>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-th"></i>
+                            <span>Menu</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{url('/admin/menu')}}"><i class="fa fa-angle-right"></i> Menu</a></li>
+                            <li><a href="{{url('/admin/menuitem')}}"><i class="fa fa-angle-right"></i> Menu item</a></li>
+                        </ul>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-th"></i>
+                            <span>Admin</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{url('/admin/users')}}"><i class="fa fa-angle-right"></i> Quản trị viên</a></li>
+                        </ul>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-th"></i>
+                            <span>Media manager</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{url('/admin/media')}}"><i class="fa fa-angle-right"></i> Media manager</a></li>
+                        </ul>
+                    </li>
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-th"></i>
+                            <span>Global settings</span>
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{url('/admin/settings')}}"><i class="fa fa-angle-right"></i> Global settings</a></li>
+                        </ul>
+                    </li>
+
+
+
+
+
                     <li class="treeview">
                         <a href="charts.html">
                             <i class="fa fa-pie-chart"></i>
